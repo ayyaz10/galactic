b2a278b0a86b96e63367176027f2e323613c590b
ayyaz10 <codementee@outlook.com>
2023-03-16 00:00:46 +0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./config/*.json",
@@ -8,7 +8,7 @@ module.exports = {
     "./snippets/*.liquid",
     "./templates/*.liquid",
     "./templates/*.json",
-    "./templates/customers/*.liquid",
+    // "./templates/customers/*.liquid",
   ],
   theme: {
     extend: {},
